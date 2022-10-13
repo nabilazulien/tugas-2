4e1fb26be32b862b1198fc93ea11dc6a5665e827
nabilazulien <nabilabasya.business@gmail.com>
2022-10-13 15:58:30 +0700

    deskripsi bangun

---
+++ mini_project1/src/DeskripsiBangunDatarRuang.java
@@ -0,0 +1,64 @@
+public class DeskripsiBangunDatarRuang {
+    public static String segitiga() {
+        String deskripsi = "Segitiga adalah poligon dengan tiga ujung dan tiga simpul";
+        return deskripsi;
+    }
+
+    public static String persegi() {
+        String deskripsi = "Persegi adalah turunan dari segi empat yang mempunyai ciri khusus keempat sisinya " +
+                "sama panjang dan keempat sudutnya siku-siku (90°)";
+        return deskripsi;
+    }
+
+    public static String persegiPanjang() {
+        String deskripsi = "Persegi panjang adalah persegi yang mempunyai ukuran panjang dengan dua pasang " +
+                "sisi sejajar dan empat sudut pada sudut siku-siku";
+        return deskripsi;
+    }
+    public static String jajarGenjang(){
+        String deskripsi = "Jajar genjang adalah bangun datar dua dimensi yang terdiri dari dua pasang sisi";
+        return deskripsi;
+    }
+    public static String belahKetupat(){
+        String deskripsi = "Belah ketupat adalah bangun datar dua dimensi yang dibentuk oleh empat buah sgitiga " +
+                "siku siku masing-masing beasr dengan sudut di hadapannya";
+        return deskripsi;
+    }
+    public static String layangLayang(){
+        String deskripsi = "Layang-layang adalah bangun datar yang dibentuk oleh dua pasang sisi yang masing-masing " +
+                "pasangannya sama panjang dan saling membentuk sudut";
+        return  deskripsi;
+    }
+    public static String trapesium(){
+        String deskripsi = "Trapesium adalah bangun datar dua dimensi yang dibentuk oleh empat buah rusuk yang dua " +
+                "di antaranya saling sejajar namun tidak sama panjang";
+        return deskripsi;
+    }
+    public static String lingkaran(){
+        String deskripsi = "Lingkaran adalah tempat kedudukan titik-titik yang berjarak sama dengan satu titik tertentu. Yang " +
+                "dimaksud titik tertentu adalah titik pusat lingkaran, sedangkan jarak yang sama adalah jari-jari lingkaran.";
+        return deskripsi;
+    }
+    public static String kubus(){
+        String deskripsi = "Kubus adalah bangun ruang tiga dimensi yang dibatasi oleh enam bidang sisi yang kongruen " +
+                "berbentuk bujur sangkar";
+        return deskripsi;
+    }
+    public static String balok(){
+        String deskripsi = "Balok adalah bangun ruang tiga dimensi yang dibentuk oleh tiga pasang persegi atau persegi panjang," +
+                " dengan paling tidak satu pasang di antaranya berukuran berbeda";
+        return deskripsi;
+    }
+    public static String tabung(){
+        String deskripsi = "Tabung atau silinder adalah bangun ruang tiga dimensi yang dibentuk oleh dua buah lingkaran " +
+                "identik yang sejajar dan sebuah persegi panjang yang mengelilingi kedua lingkaran tersebut";
+        return deskripsi;
+    }
+    public static String bola(){
+        String deskripsi = "Bola adalah objek geometri dalam ruang tiga dimensi yang merupakan permukaan dari bola, " +
+                "analog dengan objek melingkar dalam dua dimensi";
+        return deskripsi;
+    }
+
+
+}
