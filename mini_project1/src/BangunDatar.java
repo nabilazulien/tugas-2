fa592adf672f6224ffc0381af0e02541bd159dc1
nabilazulien <nabilabasya.business@gmail.com>
2022-10-13 15:57:27 +0700

    membuat bangun datar

---
+++ mini_project1/src/BangunDatar.java
@@ -0,0 +1,11 @@
+public class BangunDatar {
+    public static String listBangunDatar(){
+        String aaaa= "List command bangun datar\n1. Segitiga\n2. Persegi\n3. Persegi Panjang\n" +
+                "4. Jajar Genjang\n5. Belah Ketupat\n6. Layang-layang\n7. Trapesium\n" +
+                "8. Lingkaran\nList command bangun ruang\n" +
+                "9. Kubus\n10. Balok\n11. Tabung\n12. Bola";
+        return aaaa;
+    }
+
+
+}
